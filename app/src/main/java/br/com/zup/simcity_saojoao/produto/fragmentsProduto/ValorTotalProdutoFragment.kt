43a7a8a25ac53b5e7c1f813439ca8010dd5739f2
TCriGa <thay.c.gayv@gmail.com>
2022-06-12 15:46:34 -0300

    Atualizar Desafio -Implementar Logica ValorTotalProdutoFragment.kt

--- app/src/main/java/br/com/zup/simcity_saojoao/produto/fragmentsProduto/ValorTotalProdutoFragment.kt
+++ app/src/main/java/br/com/zup/simcity_saojoao/produto/fragmentsProduto/ValorTotalProdutoFragment.kt
@@ -1,12 +1,14 @@
 package br.com.zup.simcity_saojoao.produto.fragmentsProduto
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import br.com.zup.simcity_saojoao.R
+import androidx.fragment.app.Fragment
+import br.com.zup.simcity_saojoao.CHAVE_PRODUTO
 import br.com.zup.simcity_saojoao.databinding.FragmentValorTotalProdutoBinding
+import br.com.zup.simcity_saojoao.model.Produto
+
 
 class ValorTotalProdutoFragment : Fragment() {
     private lateinit var binding: FragmentValorTotalProdutoBinding
@@ -14,8 +16,23 @@ class ValorTotalProdutoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentValorTotalProdutoBinding.inflate(inflater,container, false )
+        binding = FragmentValorTotalProdutoBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        receberExibirDadosProdutos()
+    }
+
+    private fun receberExibirDadosProdutos() {
+        var valorTotal = 0.0
+        arguments?.getParcelableArrayList<Produto>(CHAVE_PRODUTO)?.forEach {
+            valorTotal += it.getValorProduto() * it.getQuantidade()
+
+            binding.textValorTotal.text = valorTotal.toString()
+        }
+
+
+    }
 }
