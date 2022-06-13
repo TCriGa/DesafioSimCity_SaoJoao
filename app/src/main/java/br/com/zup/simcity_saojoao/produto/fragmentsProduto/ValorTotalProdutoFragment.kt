15f2fb34ea18754f80d82ad77902ce77b719f878
TCriGa <thay.c.gayv@gmail.com>
2022-06-13 10:28:45 -0300

    Atualizar Desafio - Arrumar Layout Margin

--- app/src/main/java/br/com/zup/simcity_saojoao/produto/fragmentsProduto/ValorTotalProdutoFragment.kt
+++ app/src/main/java/br/com/zup/simcity_saojoao/produto/fragmentsProduto/ValorTotalProdutoFragment.kt
@@ -12,6 +12,7 @@ import br.com.zup.simcity_saojoao.databinding.FragmentValorTotalProdutoBinding
 import br.com.zup.simcity_saojoao.model.Produto
 
 
+
 class ValorTotalProdutoFragment : Fragment() {
     private lateinit var binding: FragmentValorTotalProdutoBinding
 
@@ -21,8 +22,13 @@ class ValorTotalProdutoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentValorTotalProdutoBinding.inflate(inflater, container, false)
+        receberArguments()
         return binding.root
 
+    }
+
+    private fun receberArguments(){
+        listaNovaProduto = arguments?.getParcelableArrayList<Produto>(CHAVE_PRODUTO) ?: ArrayList()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
